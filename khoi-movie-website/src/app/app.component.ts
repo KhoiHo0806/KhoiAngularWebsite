5285a1f029db2da65267b6690f45a4d0783a6f64
s3877653 <hotranminhkhoi@gmail.com>
2024-11-15 14:43:40 +0700

    add simple components

--- khoi-movie-website/src/app/app.component.ts
+++ khoi-movie-website/src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { MainPageComponent } from './main-page/main-page.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, LoginPageComponent, MainPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'khoi-movie-website';
+  
 }
