acd2b2b4d3b0697b6180d1d2c2d25be79426b272
s3877653 <hotranminhkhoi@gmail.com>
2024-11-17 18:01:16 +0700

    add movie slider

--- khoi-movie-website/src/app/main-page/main-page.component.ts
+++ khoi-movie-website/src/app/main-page/main-page.component.ts
@@ -1,30 +1,48 @@
 import { Component } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import {faPlay} from '@fortawesome/free-solid-svg-icons'
+import {faPlay, faChevronRight, faChevronLeft} from '@fortawesome/free-solid-svg-icons'
 import { Movie } from '../model/movie';
 import { DataService } from '../service/data.service';
+import { NgStyle } from '@angular/common';
 
 
 
 @Component({
   selector: 'app-main-page',
   standalone: true,
-  imports: [FontAwesomeModule],
+  imports: [FontAwesomeModule, NgStyle],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.css'
 })
 
 export class MainPageComponent {
 //font awesome icons
-  faPlay = faPlay
+  faPlay = faPlay;
+  faChevronRight = faChevronRight;
+  faChevronLeft = faChevronLeft;
 
   selectedMovie: Movie | null = null;
   movieList: Movie[] = [];
+
+  movieListTransformValue: number = 0;
+  movieItemWidth: number = 220;
 
   constructor(private dataService: DataService) {}
 
   ngOnInit(){
     this.movieList = this.dataService.getMovieList()
+  }
+
+  moveLeft(){
+    if(this.movieListTransformValue < 0){
+      this.movieListTransformValue += this.movieItemWidth
+    }
+  }
+
+  moveRight(){
+    if(this.movieListTransformValue > -(this.movieList.length - 5) * this.movieItemWidth){
+      this.movieListTransformValue -=this.movieItemWidth
+    }
   }
 }
 
